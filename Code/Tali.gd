f45874068d1f80b32ff1e961886b7fde55411af0
PyDario <dario.pichowsky@gmail.com>
2020-02-16 11:29:23 +0100

    tried jumping mechanics

--- Code/Tali.gd
+++ Code/Tali.gd
@@ -8,11 +8,18 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	velocity = Vector2()
+	var bool_floor = is_on_floor()
+	var _vector_dummy = Vector2()
+	if Input.is_action_pressed("jump"):
+		if is_on_floor():
+			velocity = Vector2(0, -GRAVITY)
+	else:
+		velocity = Vector2(0, GRAVITY)
 	if Input.is_action_pressed("right"):
-		velocity.x += 1
+		velocity.x += 1 * SPEED
 	if Input.is_action_pressed("left"):
-		velocity.x -= 1
-	velocity *= SPEED
-	velocity.y += GRAVITY
-	velocity = move_and_slide(velocity)
+		velocity.x -= 1 * SPEED
+	#velocity.y += GRAVITY
+	_vector_dummy = move_and_slide(velocity)
+	#print(_vector_dummy)
+	print(bool_floor)
