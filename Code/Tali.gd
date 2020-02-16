ce83d90a1eea400d1e095b12bf7593d4f00ea30c
PyDario <dario.pichowsky@gmail.com>
2020-02-16 17:40:45 +0100

    build prototypes for ledges

--- Code/Tali.gd
+++ Code/Tali.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
 const SPEED = 100
-const GRAVITY = 400
+const GRAVITY = 700
 const UP_VECTOR = Vector2(0, -1)
-const JUMP_POWER = GRAVITY / 2
+const JUMP_POWER = 200
 
 var velocity = Vector2()
 
@@ -21,14 +21,18 @@ func _physics_process(delta):
 		velocity.y += GRAVITY * delta
 	
 	player_action()
-	
+
 	_vector_dummy = move_and_slide(velocity, UP_VECTOR)
-	print(_vector_dummy)
+	if is_on_wall():
+		print("on wall")
+		
+	else:
+		print("not on wall")
+	#print(_vector_dummy)
 
 func player_action():
-	if is_on_floor():
-		if Input.is_action_pressed("jump"):
-			velocity.y = -JUMP_POWER
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		velocity.y = -JUMP_POWER
 			
 	if Input.is_action_pressed("right"):
 		velocity.x = 1 * SPEED
