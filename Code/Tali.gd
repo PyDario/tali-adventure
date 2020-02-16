abfdfba4242fc4731a6395415ea2270302361c56
PyDario <dario.pichowsky@gmail.com>
2020-02-16 13:49:47 +0100

    gravity now works as intended

--- Code/Tali.gd
+++ Code/Tali.gd
@@ -3,8 +3,11 @@ extends KinematicBody2D
 const SPEED = 100
 const GRAVITY = 400
 const UP_VECTOR = Vector2(0, -1)
-const JUMP_POWER = 200
+const JUMP_POWER = GRAVITY / 2
+
 var velocity = Vector2()
+
+#debug variablen
 var _vector_dummy = Vector2()
 
 func _ready():
@@ -12,20 +15,21 @@ func _ready():
 
 func _physics_process(delta):
 	velocity.x = 0
-	velocity.y += GRAVITY * delta
+	if is_on_floor():
+		velocity.y = 0
+	else:
+		velocity.y += GRAVITY * delta
 	
 	player_action()
 	
-	velocity.normalized()
 	_vector_dummy = move_and_slide(velocity, UP_VECTOR)
 	print(_vector_dummy)
-	#print(bool_floor)
 
 func player_action():
-	var bool_floor = is_on_floor()
 	if is_on_floor():
 		if Input.is_action_pressed("jump"):
 			velocity.y = -JUMP_POWER
+			
 	if Input.is_action_pressed("right"):
 		velocity.x = 1 * SPEED
 	if Input.is_action_pressed("left"):
